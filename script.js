c082f8dc36022b30e75209ced5f6d2648c8085d1
Kartohan <Kartohan@gmail.com>
2022-05-09 16:00:23 +0300

    Create buttons and color picker

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function createGrid(){
 let container = document.querySelector('.container');
 let grid = 16; //prompt('What size of grid do you want?')
 let size = 500/grid;
@@ -5,11 +6,52 @@ for (let i = 0; i < (grid*grid); i++) {
     let square = document.createElement('div');
     container.appendChild(square);
     square.classList = 'piece';
-    square.style.flexBasis = `${size}px`
+    square.style.flexBasis = `${size}px`;
 }
-let square = document.querySelectorAll('.piece');
-square.forEach(sq => sq.addEventListener('mouseout', () => {
-    sq.style.backgroundColor = 'blue'
-}))
-//console.log(container.addEventListener('mouseover', (e) => console.log(e)));
+container.style.border = '15px solid gray';
+}
+
+createGrid();
+let color = function color(){
+    let color = document.querySelector('.colorPicker');
+    return color.value;
+}
+
+function draw(){
+    let color = document.querySelector('.colorPicker');
+    let square = document.querySelectorAll('.piece');
+    square.forEach( sq => sq.addEventListener( 'mouseover', () =>
+    sq.style.backgroundColor = `${color.value}`
+    ))
+}
+
+
+function clear(){
+    let clear = document.querySelector('.clear')
+    clear.addEventListener('click', () => {
+        let sqaure = document.querySelectorAll('.piece')
+        sqaure.forEach(sq => sq.style.backgroundColor = 'white')
+    })
+}
+
+clear()
+
+function erase() {
+    let erase = document.querySelector('.erase')
+    erase.addEventListener('click', () => {
+        let sqaure = document.querySelectorAll('.piece');
+        sqaure.forEach(sq => sq.addEventListener ('mouseover', () => 
+        sq.style.backgroundColor = 'white'))
+    })
+}
+
+erase();
+ // pen button didn't work
+function pen() {
+    let pen = document.querySelector('.pen')
+    pen.addEventListener('click', draw())
+}
+
+pen()
+
 
